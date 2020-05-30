eb7c9dfd00357d4e80d3669c108b6ed904be7769
Chris Schultz <cschultz5923@gmail.com>
2020-05-29 22:59:19 -0600

    first files for hello triangle

---
+++ hellotriangle/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ * Chris Schultz
+ * 30 May 2020
+ *
+ * "Hello Triangle" introductory program
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "Hello, world!" << endl;
+    return 0;
+}
